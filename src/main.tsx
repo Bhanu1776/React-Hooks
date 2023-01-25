ea78d677e3eb803c674436b684bb074513ca59f2
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-01-25 19:57:29 +0530

    Moved Files from components to a useState Folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import UseState from "./Hooks/UseState";
-import Challenge1 from "./Hooks/Challenge_1";
-import UseEffect from "./Hooks/UseEffect";
+import UseState from "./Hooks/useState/UseState";
+import Challenge1 from "./Hooks/useState/Challenge_1";
+import UseEffect from "./Hooks/useEffect/UseEffect";
 import { GrandParent } from "./Hooks/useContext/GrandParent";
 import GrandChild from "./Hooks/useContext/GrandChild";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <GrandParent>
     {/* <App /> */}
-    {/* <UseState /> */}
+    <UseState />
     {/* <Challenge1 /> */}
     {/* <UseEffect /> */}
-    <GrandChild />
+    {/* <GrandChild /> */}
   </GrandParent>
 );
